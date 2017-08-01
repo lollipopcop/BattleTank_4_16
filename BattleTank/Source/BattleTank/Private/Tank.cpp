52f45b2118008ddc03549249cef5e33891ac0b31
DESKTOP-EB4C853\dylan <dylan.powell@live.ca>
2017-07-31 22:06:51 -0700

    Lecture 133 - Blueprint Callable

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -39,3 +39,7 @@ void ATank::AimAt(FVector HitLocation)
 	TankAimingComponent->AimAt(HitLocation);
 }
 
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
